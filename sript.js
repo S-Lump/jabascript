c43b0b1efbad6ae44fd448568e6c3dff46962d42
StupidLamer <grisga2001@gmail.com>
2020-10-29 10:11:21 +0800

    remove all

---
+++ sript.js
@@ -0,0 +1,10 @@
+function Car(name, year) {
+    this.name = name;
+    this.year = year;
+}
+
+
+var ford = new Car('Ford', 2015);
+var bmw = new Car('BMW', 2014);
+console.log(ford);
+console.log(bmw);

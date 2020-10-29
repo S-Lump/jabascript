c4b9d714aa803f4e246ea041df2c10fc21f1f15b
StupidLamer <grisga2001@gmail.com>
2020-10-29 10:45:50 +0800

    test prototype

--- sript.js
+++ sript.js
@@ -1,10 +1,52 @@
-function Car(name, year) {
-    this.name = name;
-    this.year = year;
+var ford = Object.create({
+    calculateDistancePerYear: function() {
+        Object.defineProperty(this, 'distancePerYear', {
+            value: Math.ceil(this.dist / this.age),
+            enumerable: true,
+            writable: false,
+            configurable: false,
+        })
+    }
+}, {
+    name: {
+        value: 'Ford',
+        enumerable:true,
+        writable: false,
+        configurable: false,
+    },
+    model: {
+        value: 'Focus',
+        enumerable:true,
+        writable: false,
+        configurable: false,
+    },
+    year: {
+        value: 2015,
+        enumerable:true,
+        writable: false,
+        configurable: false,
+    },
+    dist: {
+        value: 390000,
+        enumerable:true,
+        writable: true,
+        configurable: false,
+    },
+    age: {
+        enumerable: true,
+        get: function() {
+            return new Date().getFullYear() - this.year;
+        },
+        set: function() {
+
+        }
+    }
+});
+
+// ford.calculateDistancePerYear();
+
+for (var key in ford) {
+    if (ford.hasOwnProperty(key)) {
+        console.log(key, ford[key])
+    }
 }
-
-
-var ford = new Car('Ford', 2015);
-var bmw = new Car('BMW', 2014);
-console.log(ford);
-console.log(bmw);
